37e17a6d16f36dea0dde62494465720bf58ceaf7
US <US@A1M23>
2017-08-12 16:55:50 +0700

    kiểm tra tại lớp

--- D4_T153706_Nhut/src/notpad/MyNotePad_Nhut.java
+++ D4_T153706_Nhut/src/notpad/MyNotePad_Nhut.java
@@ -5,10 +5,12 @@ package notpad;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 import javax.swing.JColorChooser;
@@ -22,7 +24,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 /**
- * @author US
+ * @author Minh Nguyen
  *
  */
 public class MyNotePad_Nhut extends JFrame {
@@ -32,7 +34,7 @@ public class MyNotePad_Nhut extends JFrame {
 
 	JMenuBar mnbar;
 	JMenu mnuFile, mnuFormat, mnuTool;
-	JMenuItem mniOpen, mniExit, mniChangeBgColor, mniChangeFontColor;
+	JMenuItem mniOpen, mniExit, mniChangeBgColor, mniChangeFontColor, mniAnalyze;
 
 	JTextArea txaContent;
 	JScrollPane scrPane;
@@ -64,6 +66,70 @@ public class MyNotePad_Nhut extends JFrame {
 					changeBGColor();
 				} else if (e.getSource() == mniChangeFontColor) {
 					changeFontColor();
+				} else if (e.getSource() == mniAnalyze) {
+					// cach1
+					// for (String line : txaContent.getText().split("\r\n")) {
+					// System.out.print(line);
+					// }
+					int CountSv = 0;
+					int diemtu7trolen = 0;
+					int diemtu45troxuong = 0;
+					double Min = 10;
+					double Max = 0;
+					double focmax = 0;
+					String tenmin = null;
+					String tenmax = null;
+					String tenFoc = null;
+					DecimalFormat scrFormat = new DecimalFormat("0.00");
+					String[] lines = txaContent.getText().split("\r\n");
+					for (int i = 1; i < lines.length; i++) {
+						// System.out.print(lines[i]);
+						String[] fields = lines[i].split(",");
+						if (fields.length == 7) {
+							String stt = fields[0];
+							String sid = fields[1];
+							String lastName = fields[2];
+							String firstName = fields[3];
+							int foc = Integer.parseInt(fields[4]);
+							int introNW = Integer.parseInt(fields[5]);
+							int adp = Integer.parseInt(fields[6]);
+							// diem trung binh
+							CountSv++;
+							double average = (foc + introNW + adp) * 1.0 / 3;
+							DecimalFormat scoreFormat = new DecimalFormat("0.00");
+							System.out.println(scoreFormat.format(average));
+							if (average >= 7) {
+								diemtu7trolen++;
+							}
+							if (average < 4.5) {
+								diemtu45troxuong++;
+							}
+							if (average < Min) {
+								Min = average;
+								tenmin = lastName + firstName;
+								
+							}
+							if (average > Max) {
+								Max = average;
+								tenmax = lastName + firstName;
+							}
+							if (foc>=focmax){
+								focmax = foc;
+								tenFoc = lastName + firstName;
+							}
+						}
+
+					}
+					String message = "so luong sinh vien : "+ CountSv+ "\n";
+					message += "so luong sinh vien tren 7.0:" + diemtu7trolen +"\n";
+					message += "so luong sinh vien duoi 4.5:" + diemtu45troxuong +"\n";
+					message += "ten sinh vien co dtb nho nhat: " + tenmin +"\n";
+					message += "diem sinh vien co dtb nho nhat: " + Min +"\n";
+					message += "ten sinh vien co dtb lon nhat: " + tenmax +"\n";
+					message += "diem sinh vien co dtb lon nhat: " + scrFormat.format(Max) +"\n";
+					message += "ten sinh vien co Foc lon nhat: " + tenFoc +"\n";
+					message += "diem sinh vien Foc lon nhat: " + focmax +"\n";
+					JOptionPane.showConfirmDialog(MyNotePad_Nhut.this, message);
 				}
 			}
 		};
@@ -71,6 +137,7 @@ public class MyNotePad_Nhut extends JFrame {
 		mniExit.addActionListener(action);
 		mniChangeBgColor.addActionListener(action);
 		mniChangeFontColor.addActionListener(action);
+		mniAnalyze.addActionListener(action);
 		this.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
@@ -83,20 +150,23 @@ public class MyNotePad_Nhut extends JFrame {
 		mnbar = new JMenuBar();
 		mnuFile = new JMenu("File");
 		mnuFormat = new JMenu("Format");
+		mnuTool = new JMenu("Tool");
 		mniOpen = new JMenuItem("Open");
 		mniExit = new JMenuItem("Exit");
+		mniAnalyze = new JMenuItem("Analyze");
 		mniChangeBgColor = new JMenuItem("ChangeBgColor");
 		mniChangeFontColor = new JMenuItem("ChangeFontColor");
 
 		mnuFile.add(mniOpen);
 		mnuFile.addSeparator();
 		mnuFile.add(mniExit);
-
+		mnuTool.add(mniAnalyze);
 		mnuFormat.add(mniChangeBgColor);
 		mnuFormat.add(mniChangeFontColor);
 
 		mnbar.add(mnuFile);
 		mnbar.add(mnuFormat);
+		mnbar.add(mnuTool);
 
 		setJMenuBar(mnbar);
 	}
@@ -109,7 +179,7 @@ public class MyNotePad_Nhut extends JFrame {
 			try {
 				Scanner scn = new Scanner(fchOChooser.getSelectedFile());
 				while (scn.hasNextLine()) {
-					txaContent.append(scn.nextLine() + "\n");
+					txaContent.append(scn.nextLine() + "\r\n");
 
 				}
 				scn.close();
